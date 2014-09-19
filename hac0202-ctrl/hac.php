8f94c422a4f55ec066335fff4584d872c26ec96c
balazsgrill <balage.g@gmail.com>
2014-09-19 21:24:29 +0000

    control page in php

---
+++ hac0202-ctrl/hac.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a class="button" href="?actionid="0"> 0% </a>
+
+{{button label=" 0% " action="hac0202ctrl 1 0"}}
+{{button label=" 25% " action="hac0202ctrl 1 64"}}
+{{button label=" 50% " action="hac0202ctrl 1 128"}}
+{{button label=" 100% " action="hac0202ctrl 1 255"}}
+<br>
+{{button label=" ON " action="hac0202ctrl 0 8"}}
+{{button label=" OFF " action="hac0202ctrl 0 4"}}
+
+</body>
+</html>
